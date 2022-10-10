b4919a5add9451d46db13f31688e446c2c7918d0
Ameer-Hamza2348 <ameer.hamza.0912309123@gmail.com>
2022-10-10 15:07:41 +0500

    tried to make contact form work correctly but still working in progress

--- assets/vendor/php-email-form/message.php
+++ assets/vendor/php-email-form/message.php
@@ -4,22 +4,22 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-if(!empty($email) && !empty($message)){
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $receiver = "ameer.hamza.0912309123@gmail.com";
-        $subject ="Form: $name <$email>";
-        $body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage: $message";
-        $sender = "Form: $email";  
-        if(mail($receiver, $subject, $body, $sender)){
+// if(!empty($email) && !empty($message)){
+//     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+//         $receiver = "ameer.hamza.0912309123@gmail.com";
+//         $subject ="Form: $name <$email>";
+//         $body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage: $message";
+//         $sender = "Form: $email";  
+//         if(mail($receiver, $subject, $body, $sender)){
              
-        }else{
-            echo "Sorry, Failed to Send the message!!";
-        }
-    }else{
-        echo "Enter a Valid Email Address";
-    }
-}
-else{
-echo "Email and Message field is required";
-}
+//         }else{
+//             echo "Sorry, Failed to Send the message!!";
+//         }
+//     }else{
+//         echo "Enter a Valid Email Address";
+//     }
+// }
+// else{
+// echo "Email and Message field is required";
+// }
 ?>
--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,30 +1,20 @@
 // contact form validation //
 
+const form = document.querySelector("form"),
+  statusTXT = form.querySelector(".button-area span");
 
- const form = document.querySelector("form"),
- statusTXT = form.querySelector(".button-area span");
-
- form.onsubmit = (e) => {
+form.onsubmit = (e)=>{
   e.preventDefault();
   statusTXT.style.color = "#0D6EFD";
   statusTXT.style.display = "block";
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "message.php", true);
-  xhr.onload = () => {
-   if(xhr.readyState == 4 && xhr.status == 200){
-    let response = xhr.response;
-    if(response.indexOf("Email and Message field is required") != -1 || response.indexOf("Enter a Valid Email Address") || response.indexOf("Sorry, Failed to Send the message!!")){
-      statusTXT.style.color = "red";
-    }else{
-      form.reset();
-      setTimeout(()=>{
-
-      }, 3000);
+  xhr.onload = ()=>{
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let response = xhr.response;
+      console.log(response);
     }
-    statusTXT.innerText = response;
-   }                                               
   }
   let formData = new FormData(form);
   xhr.send(formData);
 }
-
