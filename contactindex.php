48d90fa8dccb4de712c3899e93bb8e1573005db0
Ameer-Hamza2348 <ameer.hamza.0912309123@gmail.com>
2022-10-26 20:58:42 +0500

    theme changed totally

--- contactindex.php
+++ contactindex.php
@@ -13,9 +13,7 @@
   <link href="/assets/img/Tricloud-tech-favicon.png" rel="icon">
 
   <!-- Google Fonts -->
-  <link
-    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
@@ -43,7 +41,7 @@
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="" href="Index.html">Home</a></li>
+          <li><a href="index.html">Home</a></li>
           <li><a href="about.html">About</a></li>
           <li><a href="services.html">Services</a></li>
           <li><a href="portfolio.html">Portfolio</a></li>
@@ -63,7 +61,7 @@
         <div class="d-flex justify-content-between align-items-center">
           <h2>Contact</h2>
           <ol>
-          <li><a href="Index.html">Home</a></li>
+            <li><a href="Index.html">Home</a></li>
             <li>Contact</li>
           </ol>
         </div>
@@ -115,7 +113,7 @@
                 <div class="col-md-6 form-group">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
                 </div>
-                
+
                 <div class="col-md-6 form-group mt-3 mt-md-0">
                   <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
                 </div>
@@ -123,11 +121,11 @@
               <div class="form-group mt-3">
                 <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
               </div>
-                
+
               <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
+                <textarea class="form-control" name="message" rows="7" placeholder="Message" required></textarea>
               </div>
-              
+
               <br>
               <div class="button-area">
                 <button class="w-100 mx-auto text-center" type="submit">Send Message</button>
@@ -146,13 +144,11 @@
       </div>
     </section>
     <!-- End Contact Section -->
-    <?php include 'contact.php'; ?> 
+    <?php include 'contact.php'; ?>
     <!-- ======= Map Section ======= -->
     <section class="map mt-2">
       <div class="container-fluid p-0">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d459.24716554332065!2d74.3266802732436!3d31.50249419028749!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391905dd97935bad%3A0x80d74bd09e7e224b!2sAl-Qadeer%20heights!5e0!3m2!1sen!2s!4v1663854002010!5m2!1sen!2s"
-          frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d459.24716554332065!2d74.3266802732436!3d31.50249419028749!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391905dd97935bad%3A0x80d74bd09e7e224b!2sAl-Qadeer%20heights!5e0!3m2!1sen!2s!4v1663854002010!5m2!1sen!2s" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
       </div>
     </section><!-- End Map Section -->
 
@@ -160,10 +156,19 @@
 
   <!-- ======= Footer ======= -->
   <footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
-
     <div class="footer-top">
       <div class="container">
         <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>About TriCloud Technologies</h3>
+            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies
+              darta donna mare fermentum iaculis eu non diam phasellus.</p>
+            <div class="social-links mt-3">
+              <a href="https://www.facebook.com/TriCloudTechnologies" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="https://www.linkedin.com/company/tricloud-technologies/?viewAsMember=true" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
             <h4>Useful Links</h4>
@@ -198,31 +203,18 @@
 
           </div>
 
-          <div class="col-lg-3 col-md-6 footer-info">
-            <h3>About TriCloud Technologies</h3>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies
-              darta donna mare fermentum iaculis eu non diam phasellus.</p>
-            <div class="social-links mt-3">
-              <a href="https://www.facebook.com/TriCloudTechnologies" class="facebook"><i
-                  class="bx bxl-facebook"></i></a>
-              <a href="https://www.linkedin.com/company/tricloud-technologies/?viewAsMember=true" class="linkedin"><i
-                  class="bx bxl-linkedin"></i></a>
-            </div>
-          </div>
-
         </div>
       </div>
     </div>
+
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong><span>Tri-cloud Technology</span></strong>. All Rights Reserved
+        &copy; Copyright <strong class="text-info"><span>Tri-cloud Technology</span></strong>. All Rights Reserved
       </div>
     </div>
-  </footer>
-  <!-- End Footer -->
+  </footer><!-- End Footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-      class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
